b2bb9655d34fa07383baa9557093032d14da0d3c
Grace Kampa <kampa.g@husky.neu.edu>
2016-03-24 11:39:46 -0400

    Updated package json for openshift, turned on body parsing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var multer = require('multer');
 // install and require the mongoose library
 var mongoose = require('mongoose');
 
@@ -24,10 +25,11 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/WebAppMaker'));
 
-require("./app.js")(app, db, mongoose);
+//require("./public/assignment/server/app.js")(app, db, mongoose);
 
 // pass db and mongoose reference to server side application module
 require("./public/project/server/app.js")(app, db, mongoose);
