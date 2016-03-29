e0f47f8d5a22bad7a1024de411e78a6b2140afd4
Grace Kampa <kampa.g@husky.neu.edu>
2016-03-28 23:39:01 -0400

    bug fix

--- public/assignment/server/app.js
+++ public/assignment/server/app.js
@@ -2,8 +2,8 @@
  * Created by OWNER on 3/10/2016.
  */
 module.exports = function (app) {
-    var userModel = require("./models/user.model.server.js")(db, mongoose);
-    var formModel = require("./models/form.model.server.js")(db, mongoose);
+    var userModel = require("./models/user.model.server.js")(mongoose);
+    var formModel = require("./models/form.model.server.js")(mongoose);
     //var fieldModel = require("./models/field.model.server.js")();
 
     var userService = require("./services/user.service.server.js")(app, formModel, userModel);
